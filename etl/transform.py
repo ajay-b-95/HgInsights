7bede38863c073b4be8e6f6d4d19388973d5557d
ajay-b-95 <67275592+ajay-b-95@users.noreply.github.com>
2025-06-09 21:45:43 +0530

    Update transform.py

--- etl/transform.py
+++ etl/transform.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import psycopg2
 import matplotlib.pyplot as plt
+import plotly.express as px
+from sqlalchemy import create_engine
 import hashlib
 import os
 
@@ -8,14 +10,21 @@ import os
 def anonymize(value):
     return hashlib.sha256(str(value).encode()).hexdigest()
 
-conn = psycopg2.connect(
-    host='postgres',
-    database='postgres',
-    user='postgres',
-    password='postgres'
-)
+# conn = psycopg2.connect(
+#     host='postgres',
+#     database='postgres',
+#     user='postgres',
+#     password='postgres'
+# )
 
-df = pd.read_sql("SELECT * FROM staging.customers", conn)
+
+engine = create_engine('postgresql+psycopg2://postgres:postgres@postgres:5432/postgres')
+
+
+df = pd.read_csv('/data/telecom_churn.csv')
+print('df to show')
+print(df)
+
 
 #Basic Checks
 print(df.info())
@@ -42,9 +51,6 @@ df['Tenure'].fillna(df['Tenure'].median(), inplace=True)
 df['MonthlyCharges'].fillna(df['MonthlyCharges'].median(), inplace=True)
 df['TotalCharges'].fillna('0', inplace=True) 
 
-df['Age'] = df['Phone'].apply(anonymize)
-df['Gender'] = df['Name'].apply(anonymize)
-
 
 # compute the KPIs 
 churn_rate = df['Churn'].value_counts(normalize=True)['Yes'] * 100
@@ -53,18 +59,9 @@ print(f"Churn Rate: {churn_rate:.2f}%")
 avg_revenue = df['MonthlyCharges'].mean()
 print(f"Average Monthly Charges: ₹{avg_revenue:.2f}")
 
-#KPI -1
-df.groupby('ContractType')['Churn'].value_counts(normalize=True).unstack().plot(kind='bar', stacked=True)
-plt.title('Churn by Contract Type')
-plt.ylabel('Proportion')
-plt.show()
-
-
-#KPI- 2
-fig = px.scatter(df, x='Tenure', y='MonthlyCharges', color='Churn',
-                 title='Tenure vs Monthly Charges colored by Churn')
-fig.show()
 
 
 #write the updated dataframe to reporting schema in postgre
-df.to_sql("customers", conn, schema='reporting', if_exists='replace', index=False)
+# df.to_sql("customers", conn, schema='reporting', if_exists='replace', index=False)
+# Use engine (not raw conn)
+df.to_sql("customers", engine, schema='reporting', if_exists='replace', index=False)
