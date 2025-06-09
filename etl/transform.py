46dfbe68410babd52be5a49369079ba234f3af6f
ajay-b-95 <67275592+ajay-b-95@users.noreply.github.com>
2025-06-09 16:01:40 +0530

    Update transform.py

--- etl/transform.py
+++ etl/transform.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import psycopg2
+import matplotlib.pyplot as plt
 import hashlib
 import os
+
 
 def anonymize(value):
     return hashlib.sha256(str(value).encode()).hexdigest()
@@ -15,7 +17,12 @@ conn = psycopg2.connect(
 
 df = pd.read_sql("SELECT * FROM staging.customers", conn)
 
-# Handle missing values
+#Basic Checks
+print(df.info())
+print(df.describe())
+print(df.isnull().sum())
+
+# Handle missing values (we can impute the standard values if givn by business
 df.fillna({
     'CustomerID': 'unknown_id',
     'Age': '0',
@@ -29,7 +36,35 @@ df.fillna({
     'Churn': 'Unknown'
 }, inplace=True)
 
+#handling attributes with numbers 
+df['Age'].fillna(df['Age'].median(), inplace=True)
+df['Tenure'].fillna(df['Tenure'].median(), inplace=True)
+df['MonthlyCharges'].fillna(df['MonthlyCharges'].median(), inplace=True)
+df['TotalCharges'].fillna('0', inplace=True) 
+
 df['Age'] = df['Phone'].apply(anonymize)
 df['Gender'] = df['Name'].apply(anonymize)
 
+
+# compute the KPIs 
+churn_rate = df['Churn'].value_counts(normalize=True)['Yes'] * 100
+print(f"Churn Rate: {churn_rate:.2f}%")
+
+avg_revenue = df['MonthlyCharges'].mean()
+print(f"Average Monthly Charges: ₹{avg_revenue:.2f}")
+
+#KPI -1
+df.groupby('ContractType')['Churn'].value_counts(normalize=True).unstack().plot(kind='bar', stacked=True)
+plt.title('Churn by Contract Type')
+plt.ylabel('Proportion')
+plt.show()
+
+
+#KPI- 2
+fig = px.scatter(df, x='Tenure', y='MonthlyCharges', color='Churn',
+                 title='Tenure vs Monthly Charges colored by Churn')
+fig.show()
+
+
+#write the updated dataframe to reporting schema in postgre
 df.to_sql("customers", conn, schema='reporting', if_exists='replace', index=False)
