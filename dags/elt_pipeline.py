1c3b5e14853944824172663b4fe3373073a2c289
ajay-b-95 <67275592+ajay-b-95@users.noreply.github.com>
2025-06-09 12:12:57 +0530

    Create elt_pipeline.py

---
+++ dags/elt_pipeline.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+from datetime import timedelta
+
+dag = DAG(
+    'elt_hourly_pipeline',
+    schedule_interval='@hourly',
+    start_date=days_ago(1),
+    catchup=False,
+    default_args={'retries': 1, 'retry_delay': timedelta(minutes=5)}
+)
+
+extract = BashOperator(
+    task_id='load_csv',
+    bash_command='psql -h postgres -U postgres -d postgres -c "\copy staging.customers FROM \'/data/telecom_churn.csv\' DELIMITER \',\' CSV HEADER;"',
+    dag=dag
+)
+
+transform = BashOperator(
+    task_id='transform_load',
+    bash_command='python3 /etl/transform.py',
+    dag=dag
+)
+
+extract >> transform
