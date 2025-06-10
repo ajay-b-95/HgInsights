573d4daf61d09be26b0fbe1ccde0a270998ac4a8
ajay-b-95 <67275592+ajay-b-95@users.noreply.github.com>
2025-06-10 17:35:26 +0530

    Update init.sql

--- init/init.sql
+++ init/init.sql
@@ -1,15 +1,2 @@
 CREATE SCHEMA IF NOT EXISTS staging;
 CREATE SCHEMA IF NOT EXISTS reporting;
-
-CREATE TABLE IF NOT EXISTS staging.customers(
-    CustomerID TEXT,
-    Age TEXT,
-    Gender TEXT,
-    Tenure TEXT,
-    MonthlyCharges TEXT,
-    ContractType TEXT,
-    InternetService TEXT,
-    TotalCharges TEXT,
-    TechSupport TEXT,
-    Churn TEXT
-);
